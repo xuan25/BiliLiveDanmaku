9f3a36bd223d7959ae4963453161e7aaeabcc946
瑄 <shanboxuan@me.com>
2021-02-09 01:13:37 +0000

    Rearrange files

--- BiliLiveDanmaku/BiliLiveDanmaku/Modules/ModuleBorder.xaml.cs
+++ BiliLiveDanmaku/BiliLiveDanmaku/Modules/ModuleBorder.xaml.cs
@@ -3,7 +3,7 @@ using System;
 using System.Windows;
 using System.Windows.Controls;
 
-namespace BiliLiveDanmaku
+namespace BiliLiveDanmaku.Modules
 {
     /// <summary>
     /// Interaction logic for ModuleBorder.xaml
